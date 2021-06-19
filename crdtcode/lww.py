bbaa2678edfad7723b29e92c0fc21426158da93f
LAPTOP-LPC24RR9\kankw <kankwamy2@gmail.com>
2021-06-19 18:54:07 +0800

    change payload to graph

--- crdtcode/lww.py
+++ crdtcode/lww.py
@@ -6,88 +6,88 @@ from datetime import datetime
 
 class SBLWWFunctions:
     """
-    This is a class to construct the LWWElementSet
+    This is a class to construct the LWWElementGraph
     """
 
     @staticmethod
-    def update(payload, vertex):
+    def update(graph, vertex):
         """
-        This function will add an vertex to Payload
-        Payload : type = list
+        This function will add an vertex to graph
+        graph : type = list
         vertex : the vertex we will be adding to the list
 
         Result:
-            return payload after a vertex is added
+            return graph after a vertex is added
         """
         vertex_in_graph = False
 
-        for i in range(len(payload)):
+        for i in range(len(graph)):
             # if vertex is in the graph, update the timestamp
-            if payload[i]['vertex'] == vertex:
-                payload[i]['timestamp'] = datetime.now()
+            if graph[i]['vertex'] == vertex:
+                graph[i]['timestamp'] = datetime.now()
                 vertex_in_graph = True
 
-        # if not in the graph, append it into the payload list
+        # if not in the graph, append it into the graph list
         if not vertex_in_graph:
-            payload.append({'vertex': vertex, 'timestamp': datetime.now()})
+            graph.append({'vertex': vertex, 'timestamp': datetime.now()})
 
-        payload.sort(key=lambda i: i['vertex'])
+        graph.sort(key=lambda i: i['vertex'])
 
-        return payload
+        return graph
 
     @staticmethod
-    def compare(payload1, payload2):
+    def compare(graph1, graph2):
         """
-        This function will compare two payload list
+        This function will compare two graph list
 
         Result:
             bool: True if two list are equal, False otherwise
         """
 
-        for element_1 in payload1:
-            if element_1 not in payload2:
+        for element_1 in graph1:
+            if element_1 not in graph2:
                 return False
         return True
 
     @staticmethod
-    def merge(payload1, payload2):
+    def merge(graph1, graph2):
         """
-        This function will merge payload 2 to payload 1
+        This function will merge graph 2 to graph 1
 
         Result:
-            return payload1 after the merging process
+            return graph1 after the merging process
         """
-        for element2 in payload2:
+        for element2 in graph2:
 
-            # check if vertex is in payload 1
+            # check if vertex is in graph 1
             vertex_found = False
 
-            for i, element1 in enumerate(payload1):
+            for i, element1 in enumerate(graph1):
                 # If element2's vertex is present and its timestamp is greater than that of element1,
                 # update the timestamp
                 if element1['vertex'] == element2['vertex']:
                     vertex_found = True
 
                     if element1['timestamp'] < element2['timestamp']:
-                        payload1[i]['timestamp'] = element2['timestamp']
+                        graph1[i]['timestamp'] = element2['timestamp']
 
-            # If element2 is not in the list, append it to payload1
+            # If element2 is not in the list, append it to graph1
             if not vertex_found:
-                payload1.append(element2)
+                graph1.append(element2)
 
-        payload1.sort(key=lambda i: i['vertex'])
-        return payload1
+        graph1.sort(key=lambda i: i['vertex'])
+        return graph1
 
     @staticmethod
-    def display(name, payload):
+    def display(name, graph):
         """
-        This function will print payload
+        This function will print graph
 
         """
 
         print("{}: ".format(name), end="")
         # Prints vertex with timestamps in microseconds
-        for item in payload:
+        for item in graph:
             print("{}:{}".format(item["vertex"], item["timestamp"].microsecond), end=", ")
 
         # print an empty line
@@ -104,24 +104,24 @@ class LWWElementSet:
 
     def add(self, elem):
         """
-        This function will add the vertex to payload W
+        This function will add the vertex to graph W
         """
 
         self.W = self.lwwf.update(self.W, elem)
 
     def remove(self, elem):
         """
-        This function will remove the vertex from payload R
+        This function will remove the vertex from graph R
         """
 
         self.R = self.lwwf.update(self.R, elem)
 
     def query(self, elem):
         """
-        This function will check if the element is in the graph/payload
+        This function will check if the element is in the graph
 
-        Result: bool: True if element is in the payload 'W'' with latest timestamp when compared to the one in
-        payload 'R', False otherwise.
+        Result: bool: True if element is in the graph 'W'' with latest timestamp when compared to the one in
+        graph 'R', False otherwise.
         """
 
         elem_in_a = [item for item in self.W if item['vertex'] == elem]
@@ -133,33 +133,32 @@ class LWWElementSet:
 
     def compare(self, lww):
         """
-        This function is to compare the two payload W and R
+        This function is to compare the two graph W and R
 
         Result:
-            return true if both payload are the same, else return false
+            return true if both graph are the same, else return false
         """
 
         return self.lwwf.compare(self.W, lww.W) and self.lwwf.compare(self.R, lww.R)
 
     def merge(self, lww):
         """
-        This function will merge two payload W and R
+        This function will merge two graph W and R
         """
 
-        # Merge payload 'W'
+        # Merge graph 'W'
         self.W = self.lwwf.merge(self.W, lww.W)
 
-        # Merge payload 'R'
+        # Merge graph 'R'
         self.R = self.lwwf.merge(self.R, lww.R)
 
     def display(self):
         """
-        This function will print/ display the two payload
+        This function will print/ display the two graph
         """
 
-        # print payload 'W'
+        # print graph 'W'
         self.lwwf.display('W', self.W)
 
-        # print payload 'R'
+        # print graph 'R'
         self.lwwf.display('R', self.R)
-
